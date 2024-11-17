8fb73c4329e4db35c6af3698c241af5f626f606d
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-17 23:43:32 +0200

    frontend <> creating signUp page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,16 +8,18 @@ import './styles/login.css'
 import './styles/base.css'
 import { Routes, Route, useLocation } from "react-router-dom";
 import Streams from './pages/Streams';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   const location = useLocation();
 
   return (
     <div className="App">
-      {location.pathname !== "/" && <NavBar />}
+      {location.pathname !== "/" && location.pathname !== "/signUp" && <NavBar /> }
 
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signUp" element={<SignUp />} />
         <Route path="/streams" element={<Streams />} />
         <Route path="/*" element={<h1>Not Found</h1>} />
       </Routes>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,29 +1,26 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
-const Login = ()=>{
+const SignUp = ()=>{
     const navigate = useNavigate();
     return(           
         <div className="flex center full-width full-height green-bg">
 
             <div className="flex column center login-container primary-bg">
-                <h1>Login</h1>
+                <h1>Sign Up</h1>
                 <input type="text" placeholder="username" className="username"/>
                 <input type="password" placeholder="password" className="password"/>
+                <input type="password" placeholder="verify password" className="password"/>
                 <button 
                 className="filled-btn green-bg white-txt"
                 onClick={()=>{
-                    navigate("/streams")
+                    navigate("/")
                 }}
-                >Login</button>
-                <button className="filled-btn  green-bg white-txt"
-                onClick={()=>{
-                    navigate("/signUp")
-                }}
-                >Sign up</button>
+                >Save</button>
+                
             </div>
         </div> 
 
     )
 }
 
-export default Login;
+export default SignUp;
