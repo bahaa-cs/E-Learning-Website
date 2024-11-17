8760438d892c186039b38764013dab4fb3345c18
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-17 23:23:36 +0200

    frontend <> navigating to streams page on login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react"
-
+import { useNavigate } from "react-router-dom";
 const Login = ()=>{
+    const navigate = useNavigate();
     return(           
         <div className="flex center full-width full-height green-bg">
 
@@ -8,7 +9,12 @@ const Login = ()=>{
                 <h1>Login</h1>
                 <input type="text" placeholder="username" className="username"/>
                 <input type="password" placeholder="password" className="password"/>
-                <button className="filled-btn">Login</button>
+                <button 
+                className="filled-btn"
+                onClick={()=>{
+                    navigate("/Streams")
+                }}
+                >Login</button>
                 <button className="filled-btn">Sign up</button>
             </div>
         </div> 
