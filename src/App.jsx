19653f9decd8a0b010f83cab3ca8e46a8f2f0dd6
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-17 22:33:53 +0200

    frontend <> adding css files

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+
+
+import { Routes, Route, useLocation } from "react-router-dom";
+
+const App = () => {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      {location.pathname !== "/" && <NavBar />}
+
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
+
