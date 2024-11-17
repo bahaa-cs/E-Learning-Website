25b55f7404221b7aa02c3a362bb64d8d6cf6f97d
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-18 01:51:06 +0200

    frontend <> adding use states in sign up page to insert students

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,19 +1,47 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const SignUp = ()=>{
+    const [registerForm,setRegisterForm] = useState({
+        "username":"",
+        "password":""
+        })
+
     const navigate = useNavigate();
     return(           
         <div className="flex center full-width full-height green-bg">
 
             <div className="flex column center login-container primary-bg">
                 <h1>Sign Up</h1>
-                <input type="text" placeholder="username" className="username"/>
-                <input type="password" placeholder="password" className="password"/>
-                <input type="password" placeholder="verify password" className="password"/>
+                <input type="text" placeholder="username" className="username"
+                onChange={(e)=>{
+                    setRegisterForm({
+                        ...registerForm,
+                        username:e.target.value
+                    })
+                }}
+                />
+                <input type="password" placeholder="password" className="password"
+                onChange={(e)=>{
+                    setRegisterForm({
+                        ...registerForm,
+                        password:e.target.value
+                    })
+                }}
+                />
+                {/* <input type="password" placeholder="verify password" className="password"/> */}
                 <button 
                 className="filled-btn green-bg white-txt"
                 onClick={()=>{
-                    navigate("/")
+                    const data = new FormData()
+                    data.append("username",registerForm.username)
+                    data.append("password",registerForm.password)
+                    axios.post("http://localhost:8080/e-learning-website/backend/insertStudent.php",data)
+                    .then(()=>{
+                        navigate("/")
+                    })
+
                 }}
                 >Save</button>
                 
