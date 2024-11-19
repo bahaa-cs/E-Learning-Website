7fd0fc1cdc8f4e390a992eecb242b1a352bee1f9
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-20 01:59:54 +0200

    frontend <> adding unenroll condition on courses

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, {useState,useEffect, useDebugValue} from "react"
+import axios from "axios"
+const Courses = ()=>{
 
-const Courses = () =>{
+    const [disabled,setDisabled] = useState(false);
+    const [courses,setCourses] = useState([]);
+
+    useEffect(()=>{
+        axios.get("http://localhost:8080/e-learning-website/backend/selectCourses.php", {
+            headers: {
+                Authorization:localStorage.token,
+            },
+        })
+        .then((response)=>{
+            setCourses(response.data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+        
+    },[]);
+
+
+    const listCourses = courses.length>0 ? courses.map((course)=>(
+        <div className="flex column center course-card primary-bg" key={course.id}>
+            <h2>{course.courseName}</h2>
+            <button 
+                className={`filled-btn green-bg white-txt ${course.isEnrolled ? "unenroll-btn" : ""}`} 
+                disabled={course.isEnrolled}>
+                {course.isEnrolled ? "Unenroll" : "Enroll Course"}
+            </button>        
+        </div>
+    )) : <div>No Courses Avaliable</div>
     return (
-        <div>Courses List</div>
+        <div className="flex row courses-container">
+            {listCourses}
+        </div>
     )
-
 }
 
 export default Courses;
