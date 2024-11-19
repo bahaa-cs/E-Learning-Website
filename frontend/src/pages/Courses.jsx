971266296c7ce2d94eee33b4c66d3d0588de0ce6
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-19 02:17:18 +0200

    frontend <> mapping courses from db to client

--- frontend/src/pages/Courses.jsx
+++ frontend/src/pages/Courses.jsx
@@ -1,13 +1,28 @@
-import React from "react"
-
+import React, {useState,useEffect} from "react"
+import axios from "axios"
 const Courses = ()=>{
+
+    const [courses,setCourses] = useState();
+
+    useEffect(()=>{
+        axios.get("http://localhost:8080/e-learning-website/backend/selectCourseStreams.php")
+        .then((res)=>{
+            setCourses(res.data);
+            console.log(courses)
+        })
+        
+    },[]);
+
+    const listCourses = courses && courses.map((course)=>(
+        <div className="flex column center course-card primary-bg">
+        <h2>{course.courseName}</h2>
+        <button className="filled-btn green-bg white-txt">Enroll Course</button>
+        <button className="filled-btn green-bg white-txt">View Course Stream</button>
+        </div>
+    ))
     return (
         <div className="flex row courses-container">
-            <div className="flex column center course-card primary-bg">
-                <h2>Course Name</h2>
-                <button className="filled-btn green-bg white-txt">Enroll Course</button>
-                <button className="filled-btn green-bg white-txt">View Course Stream</button>
-            </div>
+            {listCourses}
         </div>
     )
 }
