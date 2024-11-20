1e62177b4cfa6762c04f37413cb29bbf775b89d8
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-20 12:03:47 +0200

    backend <> editing instructors api to select id

--- backend/selectInstructorsOfStream.php
+++ backend/selectInstructorsOfStream.php
@@ -6,7 +6,7 @@ include "connection.php" ;
 
 $courses_streams_id = $_POST["courses_streams_id"];
 
-$query = $connection->prepare("select u.username from users 
+$query = $connection->prepare("select u.id,u.username from users 
                                 u inner join streams_instructors si 
                                 on u.id=si.users_id where si.courses_streams_id=?");
 $query->bind_param("i",$courses_streams_id);
