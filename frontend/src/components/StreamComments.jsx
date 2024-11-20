279f53ae423db9ada27e9305783faf9cbdb9bca4
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-20 23:51:52 +0200

    frontend <> getting comments through useContext

--- frontend/src/components/StreamComments.jsx
+++ frontend/src/components/StreamComments.jsx
@@ -3,19 +3,21 @@ import React, { useEffect,useState,useContext } from "react";
 import StreamContext from "./../contexts/StreamContext";
 
 const StreamComments = ()=>{
-    const [comments,setComments] = useState()
+    const [comments,setComments] = useState([])
+
     const streamID = useContext(StreamContext);
     useEffect(()=>{
 
-        const data = new FormData()
-        data.append("courses_streams_id",streamID)
-        axios.post("http://localhost:8080/e-learning-website/backend/selectPublicComments.php",data)
-        .then((response)=>{
-            setComments(response.data)
-            console.log(comments)
-        })
-    },[])
-    
+            const data = new FormData()
+            data.append("courses_streams_id",streamID)
+            axios.post("http://localhost:8080/e-learning-website/backend/selectPublicComments.php",data)
+            .then((response)=>{
+                setComments(response.data)
+            })
+    },[streamID])
+    useEffect(() => {
+        console.log(comments);
+      }, [comments]);
     return (
         <div>Comments</div>
     )
