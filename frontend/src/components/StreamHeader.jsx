48a4d22a8f32464f274c40e520e9d8f4d1b6d499
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-20 11:57:13 +0200

    frontend <> rendering instructors names of every stream

--- frontend/src/components/StreamHeader.jsx
+++ frontend/src/components/StreamHeader.jsx
@@ -1,8 +1,11 @@
 import React,{useEffect, useState} from "react";
 import axios from "axios"
 const StreamHeader = (props)=>{
-    const [courses,setCourses] = useState([]);
+    const [course,setCourse] = useState([]);
+    const [instructors,setInstructors] = useState([]);
+
     
+    // get course name
     useEffect(()=>{
 
         const data = new FormData();
@@ -13,19 +16,38 @@ const StreamHeader = (props)=>{
             },
         })
         .then((response)=>{
-            setCourses(response.data)
+            setCourse(response.data)
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    },[])
+    // instructors
+    useEffect(()=>{
+
+        const data = new FormData();
+        data.append("courses_streams_id",props.csID)
+        axios.post("http://localhost:8080/e-learning-website/backend/selectInstructorsOfStream.php", data)
+        .then((response)=>{
+            setInstructors(response.data)
+            
         })
         .catch((error) => {
             console.log(error);
         });
     },[])
 
+    const listInstructors = instructors.length>0 ? instructors.map((i)=>(
+
+        <>{i.username} </>
+    ) 
+    ) : <></>
+
 
     return (
         <div>
-            <p>h</p>
-            <h1>{courses.courseName}</h1>
-            {/* <p>Instructors: {instructors}</p> */}
+            <h1>{course.courseName}</h1>
+            <div><h3>Instructors:</h3> {listInstructors}</div>
             </div>
     )
 }
