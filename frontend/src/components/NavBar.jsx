b1222142c130176456d5693529ab8a26cc87f0c6
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-20 04:02:10 +0200

    frontend <> creating navbar with styles

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,13 @@ import React from "react"
 const NavBar = ()=>{
 
     return (
-        <div>navbar</div>
+        <div className="flex center full-width fixed">
+
+            <div className="flex center primary-bg black-txt navbar">
+                <div>Courses</div>
+                <div>Courses Streams</div>
+            </div>
+        </div>
     )
 }
 
