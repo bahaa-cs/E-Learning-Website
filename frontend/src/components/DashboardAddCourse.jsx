23989dfddcf317ad3fcfdcbd09ff85f76a0060d9
bahaaCs <bahaaaboukhalil@gmail.com>
2024-11-21 17:38:03 +0200

    frontend <> adding section for admin to insert cours

--- frontend/src/components/DashboardAddCourse.jsx
+++ frontend/src/components/DashboardAddCourse.jsx
@@ -1,15 +1,26 @@
+import axios from "axios";
 import React, {useState,useEffect} from "react"
 
 const DashboardAddCourse = ()=>{
     const [courseName,setCourseName] = useState();
+    const [input,setInput] = useState("")
+
     return (
         <div>
-            <input type="text" placeholder="enter course name"
+            <input type="text" value={input} placeholder="enter course name"
             onChange={(e)=>{
+                setInput(e.target.value)
                 setCourseName(e.target.value)
             }}
             />
-            <button>submit</button>
+            <button
+            onClick={()=>{
+                const data = new FormData()
+                data.append("courseName",courseName)
+                axios.post("http://localhost:8080/e-learning-website/backend/insertCourse.php",data)
+                setInput("")
+            }}
+            >submit</button>
         </div>
     )
 }
